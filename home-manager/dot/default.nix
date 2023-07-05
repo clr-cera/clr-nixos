eb45805072c2b2f170f8024c84d18ed97c65dc0e
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-07-05 20:14:29 -0300

    now kitty

--- home-manager/dot/default.nix
+++ home-manager/dot/default.nix
@@ -37,9 +37,16 @@
   ];
 
   home.file = {
+    #awesome
     ".config/awesome/rc.lua".source = config.lib.file.mkOutOfStoreSymlink ./files/awesome/rc.lua;
     ".config/awesome/clrawesome.lua".source = config.lib.file.mkOutOfStoreSymlink ./files/awesome/clrawesome.lua;
+    
+    #peaclock
     ".peaclock/config".source = config.lib.file.mkOutOfStoreSymlink ./files/peaclock/peaclockConfig;
+    
+    #kitty
+    ".config/kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink ./files/kitty/kitty.conf;
+    ".config/kitty/current-theme.conf".source = config.lib.file.mkOutOfStoreSymlink ./files/kitty/current-theme.conf;
 
 
 
