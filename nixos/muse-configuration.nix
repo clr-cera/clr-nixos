6a4bf6312e336f04b13a3f4d2edeb051f5a38428
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-07-07 18:01:39 -0300

    removed zen kernel

--- nixos/muse-configuration.nix
+++ nixos/muse-configuration.nix
@@ -87,7 +87,6 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-  linuxKernel.kernels.linux_zen
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
