c7a39e7f53cfdf7d8c705ce3db367d6085a8b9e8
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-07-10 21:33:21 -0300

    fixed overscale! and trying to fix btop

--- nixos/muse-configuration.nix
+++ nixos/muse-configuration.nix
@@ -136,8 +136,8 @@
  #Trying to fix overscale
   services.xserver.dpi = 144;
   environment.variables = rec {
-    GDK_SCALE = "0.8";
-    GDK_DPI_SCALE = "0.8";
+    GDK_SCALE = "0.67";
+    GDK_DPI_SCALE = "0.67";
   };
 
   # Enable the OpenSSH daemon.
