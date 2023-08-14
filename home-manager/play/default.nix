d55307e7b34dcd80bc294b81e017ad5a97bb26e5
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-08-14 16:10:12 -0300

    organizing hardware config

--- home-manager/play/default.nix
+++ home-manager/play/default.nix
@@ -9,5 +9,6 @@
   home.packages = with pkgs;
   [
   osu-lazer-bin
+  bottles
   ];
 }
--- nixos/muse-configuration.nix
+++ nixos/muse-configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./muse-hardware-configuration.nix
       ./desktop/nvidia.nix
       ./desktop/awesome.nix
       ./common/default.nix
