3cc8ebf1741fa4e5e6ded529b9b1293832ffb79c
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-08-20 20:18:46 -0300

    dnspython again ksksk

--- home-manager/server/default.nix
+++ home-manager/server/default.nix
@@ -6,5 +6,6 @@
   home.packages = with pkgs;
   [
     noip
+    dig
   ];
 }
--- home-manager/studies/python.nix
+++ home-manager/studies/python.nix
@@ -31,9 +31,10 @@ let
       )
       pyttsx3
       pyaudio
-      
+
       #Networking
       dnspython
+
 
     ];
   in
