8d4a084f1a84b157e892b5e2362598cdd709654c
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-08-28 00:39:00 -0300

    little update!

--- home-manager/studies/default.nix
+++ home-manager/studies/default.nix
@@ -3,7 +3,6 @@
   imports = [
     ./python.nix
     ./c.nix
-    ./haskell.nix
     ./lvim
   ];
 
--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  # nativeBuildInputs is usually what you want -- tools you need to run
   NIX_CONFIG = "experimental-features = nix-command flakes" 
   nativeBuildInputs = with pkgs.buildPackages; [
     git home-manager
