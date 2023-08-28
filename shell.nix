5f846c8e11864801283600b12de6fb24aa551851
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-08-28 00:41:42 -0300

    fixed nix-shell

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  NIX_CONFIG = "experimental-features = nix-command flakes" 
+  NIX_CONFIG = "experimental-features = nix-command flakes";
   nativeBuildInputs = with pkgs.buildPackages; [
     git home-manager
   ]; 
