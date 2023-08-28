182d325f8a693fe45c066e4ac248f96a948b0d87
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-08-28 00:58:29 -0300

    Updating nix flake

--- flake.nix
+++ flake.nix
@@ -14,10 +14,6 @@
 
     #spicetify!
     spicetify-nix.url = "github:the-argus/spicetify-nix";
-
-    #vscode extensions!
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
-
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs: {
