e866ba9304bbf6b992ccf7737f036804630ed2f0
System administrator <root@nixos.qcow2.gz-s-1vcpu-2gb-70gb-intel-nyc3-01>
2023-10-04 20:18:55 +0000

    banshee configuration insertion

---
+++ system/hosts/banshee/bansheeHardwareConfiguration.nix
@@ -0,0 +1,6 @@
+{ modulesPath, lib, ... }:
+{
+  imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
+    (modulesPath + "/virtualisation/digital-ocean-config.nix")
+  ];
+}
