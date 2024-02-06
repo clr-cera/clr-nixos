ed0ba4ba7b38d88e0be83b64a94d0d0ff9a31dbb
clr-cera <93736542+clr-cera@users.noreply.github.com>
2024-02-05 21:22:32 -0300

    throwing window managers again to system

--- home-manager/desktop/awesome.nix
+++ home-manager/desktop/awesome.nix
@@ -1,13 +1,6 @@
 {pkgs, ...}: 
 
 {
-  xsession.windowManager.awesome = {
-    enable = true;
-    luaModules = with pkgs.luaPackages; [
-      luarocks
-      luadbi-mysql
-    ];
-  };
   home.packages = with pkgs; [
     picom
     arandr
