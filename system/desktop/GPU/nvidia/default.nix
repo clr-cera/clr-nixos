4701866b2bff44bf4e23ffcb8647df0b65b0a0ab
clr-cera <93736542+clr-cera@users.noreply.github.com>
2024-02-17 13:40:08 -0300

    update nvidia and wayland ozone

--- system/desktop/GPU/nvidia/default.nix
+++ system/desktop/GPU/nvidia/default.nix
@@ -29,14 +29,31 @@
 
     prime = {
     	offload = {
-		enable = true;
-		enableOffloadCmd = true;
-	};
-
-
-    	intelBusId = "PCI:0:2:0";
-	nvidiaBusId = "PCI:1:0:0";
+        enable = true;
+        enableOffloadCmd = true;
+      };
+    
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0"; 
 
     };
+    
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
+
+    nvidiaPersistenced = true; 
+
   };
+
+  environment.systemPackages = [
+  (pkgs.writeShellScriptBin "nvidia" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '')
+  ];
 }
--- system/desktop/GUI/windowManager/hyprland.nix
+++ system/desktop/GUI/windowManager/hyprland.nix
@@ -1,7 +1,6 @@
 {
-  inputs,
-  system,
   ...
 }: {
   programs.hyprland.enable = true;
+  #environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
