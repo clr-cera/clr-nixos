3b8892b6bf91d1ced0c9c0046d462ae9ad411bef
clr-cera <93736542+clr-cera@users.noreply.github.com>
2024-02-23 02:06:23 -0300

    updated obs

--- home-manager/desktop/softwares/obs.nix
+++ home-manager/desktop/softwares/obs.nix
@@ -1,6 +1,11 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    obs-studio
-  ];
-
+  
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
 }
--- system/common/kernel.nix
+++ system/common/kernel.nix
@@ -1,7 +1,16 @@
-{pkgs, ...}: {
+{config, pkgs, ...}: {
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.kernelParams = [
     "acpi_backlight=native"
   ];
+
+  # For obs virtual cam
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
 }
